72ec45f927b3cb6a734e409ef4f7525966731b76
Ezequiel Cura <91684822+Ezequiel-Cura@users.noreply.github.com>
2023-05-26 23:58:50 -0300

    finishing up

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -3,12 +3,13 @@ import {FaBars,FaTimes,FaGithub,FaLinkedin} from "react-icons/fa"
 import { HiOutlineMail } from 'react-icons/hi';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 import Logo from "../assets/logo.png"
+import {Link} from "react-scroll"
 
-export default function Navbar() {
+export default function Navbar({homeInView,aboutInView,skillsInView,projectsInView,contactInView}:any) {
     const [nav,setNav] = useState(false)
 
     const handleNav = ()=> setNav(!nav)
-
+    
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
         <div>
@@ -18,11 +19,31 @@ export default function Navbar() {
         {/* menu */}
         
             <ul className='hidden md:flex'>
-                <li>Home</li>
-                <li>About</li>
-                <li>Technologies</li>
-                <li>Projects</li>
-                <li>Contact</li>
+                <li>
+                    <Link to='home' smooth={true} duration={500} id='Home' style={homeInView ? {color:"lightblue",textDecoration:"Underline",textShadow:" 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #e60073, 0 0 40px #e60073, 0 0 50px #e60073, 0 0 60px #e60073, 0 0 70px #e60073"} : {}}>
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link to='about' smooth={true} duration={500} id='About' style={aboutInView ? {color:"lightblue",textDecoration:"Underline",textShadow:" 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #e60073, 0 0 40px #e60073, 0 0 50px #e60073, 0 0 60px #e60073, 0 0 70px #e60073"} : {}}>
+                        About
+                    </Link>
+                </li>
+                <li>
+                    <Link to='skills' smooth={true} duration={500} id='Skills' style={skillsInView ? {color:"lightblue",textDecoration:"Underline",textShadow:" 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #e60073, 0 0 40px #e60073, 0 0 50px #e60073, 0 0 60px #e60073, 0 0 70px #e60073"} : {}}>
+                        Skills
+                    </Link>
+                </li>
+                <li>
+                    <Link to='projects' smooth={true} duration={500} id='Projects' style={projectsInView ? {color:"lightblue",textDecoration:"Underline",textShadow:" 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #e60073, 0 0 40px #e60073, 0 0 50px #e60073, 0 0 60px #e60073, 0 0 70px #e60073"} : {}}>
+                        Projects
+                    </Link>
+                </li>
+                <li>
+                    <Link to='contact' smooth={true} duration={500} id='Contact' style={contactInView ? {color:"lightblue",textDecoration:"Underline",textShadow:" 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #e60073, 0 0 40px #e60073, 0 0 50px #e60073, 0 0 60px #e60073, 0 0 70px #e60073"} : {}}>
+                        Contact
+                    </Link>
+                </li>
             </ul>
         
 
@@ -34,41 +55,86 @@ export default function Navbar() {
         </div>
 
         {/* mobile menu */}
-        <div className={!nav ? "hidden" : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"}>
-            <ul>
-                <li className='py-6 text-4xl'>Home</li>
-                <li className='py-6 text-4xl'>About</li>
-                <li className='py-6 text-4xl'>Technologies</li>
-                <li className='py-6 text-4xl'>Projects</li>
-                <li className='py-6 text-4xl'>Contact</li>
-            </ul>
-        </div>
+        <ul
+        className={
+          !nav
+            ? 'hidden'
+            : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
+        }
+      >
+        <li className='py-6 text-4xl'>
+          <Link onClick={handleNav} to='home' smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          {' '}
+          <Link onClick={handleNav} to='about' smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          {' '}
+          <Link onClick={handleNav} to='skills' smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          {' '}
+          <Link onClick={handleNav} to='projects' smooth={true} duration={500}>
+            Projects
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          {' '}
+          <Link onClick={handleNav} to='contact' smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
+      </ul>
 
         {/* social icon */}
         <div className='flex fixed flex-col top-[35%] left-0'>
             <ul>
-                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
-                    <a href="/" className='flex justify-between items-center w-full text-gray-300'>
-                        LinkkedIn <FaLinkedin size={30}/>
+                <li className='w-[80px] md:w-[142px] lg:w-[160px] h-[40px] lg:h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600 '>
+                    <a
+                    className='flex justify-between items-center w-full text-gray-300'
+                    href='https://www.linkedin.com/in/ezequiel-cura/'
+                    target='_blank'
+                    >
+                        Linkedin <FaLinkedin size={30} className="mr-[-10px] lg:mr-0 duration-300"/>
                     </a>
                 </li>
-                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
-                    <a href="" className='flex justify-between items-center w-full text-gray-300'>
-                        GitHub <FaGithub size={30}/>
+                <li className='w-[80px] md:w-[142px] lg:w-[160px] h-[40px] lg:h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
+                    <a
+                    className='flex justify-between items-center w-full text-gray-300'
+                    href='https://github.com/Ezequiel-Cura'
+                    target='_blank'
+                    >
+                        Github <FaGithub size={30} className="mr-[-10px] lg:mr-0 duration-300"/>
                     </a>
                 </li>
-                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
-                    <a href="" className='flex justify-between items-center w-full text-gray-300'>
-                        Email <HiOutlineMail size={30}/>
+                <li className='w-[80px] md:w-[142px] lg:w-[160px] h-[40px] lg:h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
+                    <a
+                    className='flex justify-between items-center w-full text-gray-300'
+                    href='/'
+                    target='_blank'
+                    >
+                        Email <HiOutlineMail size={30} className="mr-[-10px] lg:mr-0 duration-300"/>
                     </a>
                 </li>
-                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
-                    <a href="" className='flex justify-between items-center w-full text-gray-300'>
-                        Resume <BsFillPersonLinesFill size={30}/>
+                <li className='w-[80px] md:w-[142px] lg:w-[160px] h-[40px] lg:h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
+                    <a
+                    className='flex justify-between items-center w-full text-gray-300'
+                    href='/'
+                    target='_blank'
+                    
+                    >
+                        Resume <BsFillPersonLinesFill size={30} className="mr-[-10px] lg:mr-0 duration-300"/>
                     </a>
                 </li>
             </ul>
-        </div>
+       </div>
     </div>
   )
 }
---
+++ src/data/data.ts
@@ -0,0 +1,24 @@
+
+
+
+const data:IProjects[] = [
+    {
+        id:1,
+        name:"React JS Application",
+        image:"",
+        github:"",
+        demo:"",
+        description:""
+    },
+]
+
+export default data
+
+interface IProjects{
+    id:number
+    name:string
+    image:any
+    description:string
+    github:string
+    demo:string
+}
