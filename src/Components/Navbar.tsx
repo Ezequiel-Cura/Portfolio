f88cb2b4995a1bbfb9d87ea85b64344ef7754a61
Ezequiel Cura <91684822+Ezequiel-Cura@users.noreply.github.com>
2023-06-02 19:22:43 -0300

    adding favicon

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -2,7 +2,7 @@ import React,{useState} from 'react'
 import {FaBars,FaTimes,FaGithub,FaLinkedin} from "react-icons/fa"
 import { HiOutlineMail } from 'react-icons/hi';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
-import Logo from "../assets/logo.png"
+import Logo from "../assets/EZE EC-05.svg"
 import {Link} from "react-scroll"
 
 export default function Navbar({homeInView,aboutInView,skillsInView,projectsInView,contactInView}:any) {
@@ -13,7 +13,7 @@ export default function Navbar({homeInView,aboutInView,skillsInView,projectsInVi
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
         <div>
-            <img src={Logo} alt="Logo Image" style={{width: "50px"}} />
+            <img src={Logo} alt="Logo Image" style={{width: "80px"}} />
         </div>
 
         {/* menu */}
